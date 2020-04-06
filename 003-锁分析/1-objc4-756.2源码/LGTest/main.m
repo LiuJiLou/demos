37f4c2951251b831bf7292cda911a5047f439e5f
liujilou <liujilou-ds>
2020-04-06 15:54:46 +0800

    003-锁

---
+++ 003-锁分析/1-objc4-756.2源码/LGTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LGTest
+//
+//  Created by Cooci on 2020/1/4.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        [NSObject alloc];
+        NSLog(@"99999");
+    }
+    return 0;
+}
